a61560df6e3d965c544d1ff6ae44f88d31863537
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-03 16:55:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,11 @@ class Student
   end 
   
   def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10 
+      
   end 
   
   def save
