bef1e3e7de2f661f3601cc707d5041d08979f9a2
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-03 16:59:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,17 @@ class Student
     
   end 
   
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = ?
+      LIMIT ?
+      SQL
+      
+      DB[:conn].execute(sql, x).map { | row | self.new_from_db(row) }
+    end 
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
