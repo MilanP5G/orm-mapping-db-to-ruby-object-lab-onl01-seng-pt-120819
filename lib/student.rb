48dddfcf7fccb8a47dacbf874b1fd0e0185fa4a5
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-03 16:45:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
       FROM students
     SQL
  
-    DB[:conn].execute(sql).map { | row | 
+    DB[:conn].execute(sql).map { | row | self.new_from_db(row) }
   end
 
   def self.find_by_name(name)
